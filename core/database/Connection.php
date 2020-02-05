8976a6ff6c8cf0b5a835b099aa7a6a6a1cfe8808
Marko Rusic <markousp5@gmail.com>
2020-02-05 16:21:39 +0000

    db connection utf8 charset

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -16,7 +16,7 @@ class Connection
     {
         try {
             return new PDO(
-                $config['connection'].';dbname='.$config['name'],
+                $config['connection'].';dbname='.$config['name'].';charset=utf8',
                 $config['username'],
                 $config['password'],
                 $config['options']
